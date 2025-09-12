cd2248dc6598bfce5276ef17ebdcf7b9afb6d0d2
Thieuviet <thieuviet2910vv@gmail.com>
2025-09-12 11:08:04 +0700

    xoa bot

--- diutup/diutup.js
+++
@@ -1,9 +0,0 @@
-// Hàm phát video
-function playVideo(videoSrc) {
-    const videoPlayer = document.getElementById('main-video');
-    const videoTitle = document.getElementById('video-title');
-    
-    // Gán link video vào thuộc tính src của iframe
-    videoPlayer.src = videoSrc;
-    // videoTitle.textContent = "Đang phát: " + videoTitle;
-  } 
