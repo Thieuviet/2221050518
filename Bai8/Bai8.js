88d6eefd4675f5ee2b162679151685c677d1e0f4
Thieuviet <thieuviet2910vv@gmail.com>
2025-10-03 11:23:35 +0700

    bai 10

--- Bai8/Bai8.js
+++ Bai8/Bai8.js
@@ -104,18 +104,91 @@ let myString = "Hello";
 // }
 
 
-let a = 10 ;
-let b = 9 ;
-let c = 8 ;
-let tongDiem = a*0.6 + b*0.3 + c*0.1;
-if (tongDiem >= 0 && tongDiem < 4) {
-    document.writeln("F");
-} else if (tongDiem >= 4 && tongDiem < 5) {
-    document.writeln("D");
-} else if (tongDiem >= 5 && tongDiem < 7) {
-    document.writeln("C");
-} else if (tongDiem >= 7 && tongDiem < 8) {
-    document.writeln("B");
-} else {
-    document.writeln("A");
-}
+// let a = parseFloat(prompt("Nhập điểm A (0-10):"));
+// let b = parseFloat(prompt("Nhập điểm B (0-10):"));
+// let c = parseFloat(prompt("Nhập điểm C (0-10):"));
+
+// if ((a < 0 || a > 10) || (b < 0 || b > 10) || (c < 0 || c > 10)) {
+//     document.writeln("Điểm nhập không hợp lệ! Vui lòng nhập lại (0-10).");
+// } else {
+//     let tongDiem = a*0.6 + b*0.3 + c*0.1;
+
+//     if (tongDiem < 4) {
+//         document.writeln("F");
+//     } else if (tongDiem < 5) {
+//         document.writeln("D");
+//     } else if (tongDiem < 7) {
+//         document.writeln("C");
+//     } else if (tongDiem < 8) {
+//         document.writeln("B");
+//     } else {
+//         document.writeln("A");
+//     }
+// }
+
+// 
+
+// let n;
+// do {
+//     n = parseInt(prompt("Nhập số lượng môn học:"));
+//     if (isNaN(n) || n <= 0) {
+//         alert("Số môn học không hợp lệ, nhập lại!");
+//     }
+// } while (isNaN(n) || n <= 0);
+
+// let tong = 0;
+// for (let i = 1; i <= n; i++) {
+//     let diem;
+//     do {
+//         diem = parseFloat(prompt("Nhập điểm môn " + i + " (0-10):"));
+//         if (isNaN(diem) || diem < 0 || diem > 10) {
+//             alert("Điểm không hợp lệ, nhập lại!");
+//         }
+//     } while (isNaN(diem) || diem < 0 || diem > 10);
+//     tong += diem;
+// }
+
+// let tb = tong / n;
+
+// TODO: Viết đoạn if...else để phân loại xếp hạng
+
+
+
+// let thang = parseFloat(prompt("Nhập thang"));
+// let nam = parseFloat(prompt("Nhập nam"));
+
+// switch (thang) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         document.writeln("Thang co 31 ngay");
+//         break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         document.writeln("Thang co 30 ngay ");
+//         break;
+//     case 2:
+//         if ((nam % 4 == 0) || (nam % 400 == 0)) {
+//             document.writeln("Thang co 29 ngay ");
+//         } else {
+//             document.writeln("Thang co 28 nhay");
+//         }
+//         break;
+//     default:
+//         document.writeln("Nhap sai nhe");
+//         break;
+// }
+
+
+let danhSach = ["Dương Trần Nhật	Anh ", "Dương Vũ	Anh ", "Trần Vũ Hải	Anh ", "Bùi Ngọc	Bích ", "Phạm Quốc	Bình ", "Nguyễn Văn	Chung ", "Hoàng Mạnh	Cường ", "Ngô Hoàng	Dương ", "Phạm Văn	Đại ", "Nguyễn Đình	Đạt ", "Phạm	Đức ", "Vũ Trung	Đức ", "Phạm Hương	Giang ", "Phạm Quang	Hậu ", "Trần Văn	Hiệp ", "Nguyễn Văn	Hoàng ", "Phạm Quốc	Hoàng ", "Phạm Thị	Huế ", "Trần Công	Hùng ", "Đào Thế	Huy ", "Đỗ Quang	Huy ", "Phạm Quang	Huy ", "Phạm Thành	Khang ", "Bùi Văn	Khiêm ", "Nguyễn Tiến	Khởi ", "Đỗ Trung	Kiên ", "Vương Hà	Linh ", "Đào Quang	Lộc ", "Ngô Gia	Lộc ", "Bùi Văn	Mạnh ", "Nguyễn Văn	Mạnh ", "Phan Văn	Mạnh ", "Trương Công	Minh ", "Đỗ Thành	Nam ", "Lê Duy	Nam ", "Nguyễn Hoàng	Nam ", "Nguyễn Anh	Quân ", "Cao Thị Thu	Quyên ", "Phạm Gia	Tài ", "Nguyễn Thị Thanh	Tâm © ", "Nguyễn Đình	Thái ", "Lưu Tiến	Thành ", "Trần Văn	Thành ", "Nguyễn Thị	Thúy ", "Nguyễn Thị	Thúy ", "Bùi Duy	Tiến ", "Nguyễn Văn	Tiến ", "Đỗ Văn	Trung ", "Phạm Đan	Trường ", "Lê Thị Cẩm	Tú ", "Phan Văn	Tuân ", "Cao Thái	Tuấn ", "Nguyễn Duy	Tuấn ", "Nguyễn Huy	Tuấn ", "Thiều Văn	Việt ", "Nguyễn Kiều	Vy"];
+let demNguyen = danhSach.filter(ten => ten.startsWith("Nguyễn")).length;
+
+document.writeln("Số bạn họ Nguyễn là: " + demNguyen);
+
+alert("hello")
